a14ae6c2481f38e32f0559b61cfa48bcb3ee7ae3
enzozerega <enzo.zerega@chasacademy.se>
2020-01-09 16:05:13 +0100

    Add all pages

---
+++ pages/cars.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Car rental - Customers</title>
+</head>
+
+<body>
+
+    <header>
+        <nav id="main-menu">
+            <div class="main-menu-list">
+                <div id="logo">Car rental</div>
+                <a href="pages/customers.php"><div class="main-menu-item">Customers</div></a>
+                <a href="pages/cars.php"><div class="main-menu-item">Cars</div></a>
+                <a href="pages/checkin.php"><div class="main-menu-item">Check out car</div></a>
+                <a href="pages/checkout.php"><div class="main-menu-item">Check in car</div></a>
+                <a href="pages/history.php"><div class="main-menu-item">History</div></a>
+                <div id="logo">Customers</div>
+            </div>
+        </nav>
+    </header>
+
+    <main>
+
+    </main>
+
+    <footer>
+
+    </footer>
+</body>
+
+</html>
---
+++ pages/customers_add.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Car rental - Add customer</title>
+</head>
+
+<body>
+
+    <header>
+        <nav id="main-menu">
+            <div class="main-menu-list">
+                <div id="logo">Car rental</div>
+                <a href="customers.php"><div class="main-menu-item">Customers</div></a>
+                <a href="cars.php"><div class="main-menu-item">Cars</div></a>
+                <a href="checkin.php"><div class="main-menu-item">Check out car</div></a>
+                <a href="checkout.php"><div class="main-menu-item">Check in car</div></a>
+                <a href="history.php"><div class="main-menu-item">History</div></a>
+                <div id="logo">Add customer</div>
+            </div>
+        </nav>
+    </header>
+
+    <main>
+
+    </main>
+
+    <footer>
+
+    </footer>
+</body>
+
+</html>
